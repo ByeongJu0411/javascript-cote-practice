37d83e0ffe33c956df33440a3e1dd83793696506
ByeongJu0411 <wjsqudwn981789@gmail.com>
2026-03-29 23:20:40 +0900

    11286번 1차 제출

---
+++ backjoon/11286.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = parseInt(input[0]);
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  compare(a, b) {
+    return Math.abs(a) - Math.abs(b);
+  }
+
+  push(val) {
+    this.heap.push(val);
+    let idx = this.heap.length - 1;
+    while (idx > 0) {
+      const parent = Math.floor((idx - 1) / 2);
+      if (this.compare(this.heap[idx], this.heap[parent]) < 0) {
+        [this.heap[idx], this.heap[parent]] = [this.heap[parent], this.heap[idx]];
+        idx = parent;
+      } else {
+        break;
+      }
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 0) return 0;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    let idx = 0;
+
+    while (true) {
+      const left = idx * 2 + 1;
+      const right = idx * 2 + 2;
+      let smallest = idx;
+
+      if (left < this.heap.length && this.compare(this.heap[left], this.heap[smallest]) < 0) {
+        smallest = left;
+      }
+      if (right < this.heap.length && this.compare(this.heap[right], this.heap[smallest]) < 0) {
+        smallest = right;
+      }
+
+      if (smallest !== idx) {
+        [this.heap[idx], this.heap[smallest]] = [this.heap[smallest], this.heap[idx]];
+        idx = smallest;
+      } else {
+        break;
+      }
+    }
+
+    return min;
+  }
+}
+
+const heap = new MinHeap();
+const result = [];
+
+for (let i = 1; i <= n; i++) {
+  const x = parseInt(input[i]);
+  if (x !== 0) {
+    heap.push(x);
+  } else {
+    result.push(heap.pop());
+  }
+}
+
+console.log(result.join("\n"));
