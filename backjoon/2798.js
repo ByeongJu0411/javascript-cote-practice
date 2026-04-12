8d813c9127f1641c330af1a3d3990c418aa05770
ByeongJu0411 <wjsqudwn981789@gmail.com>
2026-04-12 11:46:37 +0900

    2798번 같은 카드를 사용하는 경우 오류를 수정 후 2차 제출

--- backjoon/2798.js
+++ backjoon/2798.js
@@ -14,8 +14,8 @@ rl.on("close", () => {
   let result = 0;
 
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      for (let k = 0; k < n; k++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j; k < n; k++) {
         const total = cards[i] + cards[j] + cards[k];
         if (total <= m) {
           result = Math.max(result, total);
