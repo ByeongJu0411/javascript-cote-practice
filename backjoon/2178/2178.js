cd565333436dbb62b6db544d428308deced31cae
ByeongJu0411 <wjsqudwn981789@gmail.com>
2026-05-17 22:01:42 +0900

    2178 미로탐색 무한루프 오류로 visited 배열 추가하여 2차 제출

--- backjoon/2178/2178.js
+++ backjoon/2178/2178.js
@@ -7,16 +7,19 @@ for (let i = 1; i <= N; i++) {
 }
 
 let answer = Infinity;
+const visited = Array.from({ length: N }, () => new Array(M).fill(false));
 
 function go(x, y, count) {
   if (x < 0 || y < 0 || x >= N || y >= M) return;
   if (maze[x][y] === 0) return;
+  if (visited[x][y]) return;
 
   if (x === N - 1 && y === M - 1) {
     answer = Math.min(answer, count);
     return;
   }
 
+  visited[x][y] = true;
   go(x - 1, y, count + 1);
   go(x + 1, y, count + 1);
   go(x, y - 1, count + 1);
