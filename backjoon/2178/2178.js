17b530ebb03f755df6a3f3e2d1b59ec597bc4072
ByeongJu0411 <wjsqudwn981789@gmail.com>
2026-05-17 21:55:27 +0900

    2178번 미로탐색 1차 풀이

---
+++ backjoon/2178/2178.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, M] = input[0].split(" ").map(Number);
+const maze = [];
+for (let i = 1; i <= N; i++) {
+  maze.push(input[i].split("").map(Number));
+}
+
+let answer = Infinity;
+
+function go(x, y, count) {
+  if (x < 0 || y < 0 || x >= N || y >= M) return;
+  if (maze[x][y] === 0) return;
+
+  if (x === N - 1 && y === M - 1) {
+    answer = Math.min(answer, count);
+    return;
+  }
+
+  go(x - 1, y, count + 1);
+  go(x + 1, y, count + 1);
+  go(x, y - 1, count + 1);
+  go(x, y + 1, count + 1);
+}
+
+go(0, 0, 1);
+console.log(answer);
